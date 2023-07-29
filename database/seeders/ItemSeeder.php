bef743c556b70a15235c62dd5b18c314dfc8d31b
SantaCruz77 <steveyamada66@gmail.com>
2023-07-29 11:55:16 +0900

    feat: 商品一覧ページを実装する

--- database/seeders/ItemSeeder.php
+++ database/seeders/ItemSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class ItemSeeder extends Seeder
 {
@@ -14,6 +15,22 @@ class ItemSeeder extends Seeder
      */
     public function run()
     {
-        //
+        DB::table('items')->insert([
+            [
+                'name' => 'カット',
+                'memo' => 'カットの詳細',
+                'price' => 4000
+            ],
+            [
+                'name' => 'カラー',
+                'memo' => 'カラーの詳細',
+                'price' => 8000
+            ],
+            [
+                'name' => 'カラー(カット込)',
+                'memo' => 'パーマ、カットの詳細',
+                'price' => 12000
+            ]
+        ]);
     }
 }
