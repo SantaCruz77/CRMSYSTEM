1126fd83f1dc8d4a3ce6a6be5cb6c4cef18f5540
SantaCruz77 <steveyamada66@gmail.com>
2023-08-01 07:53:14 +0900

    feat: ユーザーでボタンを出し分け

--- resources/js/Pages/Dashboard.vue
+++ resources/js/Pages/Dashboard.vue
@@ -1,6 +1,10 @@
 <script setup>
-import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
-import { Head } from '@inertiajs/vue3';
+import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout.vue";
+import { Head } from "@inertiajs/vue3";
+
+defineProps({
+    auth: Object,
+});
 </script>
 
 <template>
@@ -8,13 +12,23 @@ import { Head } from '@inertiajs/vue3';
 
     <AuthenticatedLayout>
         <template #header>
-            <h2 class="font-semibold text-xl text-gray-800 leading-tight">Dashboard</h2>
+            <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+                Dashboard
+            </h2>
         </template>
 
         <div class="py-12">
             <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
                 <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                     <div class="p-6 text-gray-900">You're logged in!</div>
+                    <div v-if="auth.user.name === 'ビートたけし'">
+                        <button
+                            class="my-4 flex mx-auto text-white bg-indigo-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg"
+                        >
+                            ユーザー出し分けテスト
+                        </button>
+                        <p>{{ auth }}</p>
+                    </div>
                 </div>
             </div>
         </div>
--- resources/js/Pages/Welcome.vue
+++ resources/js/Pages/Welcome.vue
@@ -1,11 +1,12 @@
 <script setup>
-import { Head, Link } from '@inertiajs/vue3';
+import { Head, Link } from "@inertiajs/vue3";
 
 defineProps({
     canLogin: Boolean,
     canRegister: Boolean,
     laravelVersion: String,
     phpVersion: String,
+    auth: Object,
 });
 </script>
 
@@ -15,12 +16,15 @@ defineProps({
     <div
         class="relative sm:flex sm:justify-center sm:items-center min-h-screen bg-dots-darker bg-center bg-gray-100 dark:bg-dots-lighter dark:bg-gray-900 selection:bg-red-500 selection:text-white"
     >
-        <div v-if="canLogin" class="sm:fixed sm:top-0 sm:right-0 p-6 text-right">
+        <div
+            v-if="canLogin"
+            class="sm:fixed sm:top-0 sm:right-0 p-6 text-right"
+        >
             <Link
                 v-if="$page.props.auth.user"
                 :href="route('dashboard')"
                 class="font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500"
-                >Dashboard</Link
+                >Dashboard aaa</Link
             >
 
             <template v-else>
@@ -79,12 +83,20 @@ defineProps({
                                 </svg>
                             </div>
 
-                            <h2 class="mt-6 text-xl font-semibold text-gray-900 dark:text-white">Documentation</h2>
+                            <h2
+                                class="mt-6 text-xl font-semibold text-gray-900 dark:text-white"
+                            >
+                                Documentation
+                            </h2>
 
-                            <p class="mt-4 text-gray-500 dark:text-gray-400 text-sm leading-relaxed">
-                                Laravel has wonderful documentation covering every aspect of the framework. Whether you
-                                are a newcomer or have prior experience with Laravel, we recommend reading our
-                                documentation from beginning to end.
+                            <p
+                                class="mt-4 text-gray-500 dark:text-gray-400 text-sm leading-relaxed"
+                            >
+                                Laravel has wonderful documentation covering
+                                every aspect of the framework. Whether you are a
+                                newcomer or have prior experience with Laravel,
+                                we recommend reading our documentation from
+                                beginning to end.
                             </p>
                         </div>
 
@@ -125,12 +137,19 @@ defineProps({
                                 </svg>
                             </div>
 
-                            <h2 class="mt-6 text-xl font-semibold text-gray-900 dark:text-white">Laracasts</h2>
+                            <h2
+                                class="mt-6 text-xl font-semibold text-gray-900 dark:text-white"
+                            >
+                                Laracasts
+                            </h2>
 
-                            <p class="mt-4 text-gray-500 dark:text-gray-400 text-sm leading-relaxed">
-                                Laracasts offers thousands of video tutorials on Laravel, PHP, and JavaScript
-                                development. Check them out, see for yourself, and massively level up your development
-                                skills in the process.
+                            <p
+                                class="mt-4 text-gray-500 dark:text-gray-400 text-sm leading-relaxed"
+                            >
+                                Laracasts offers thousands of video tutorials on
+                                Laravel, PHP, and JavaScript development. Check
+                                them out, see for yourself, and massively level
+                                up your development skills in the process.
                             </p>
                         </div>
 
@@ -172,12 +191,19 @@ defineProps({
                                 </svg>
                             </div>
 
-                            <h2 class="mt-6 text-xl font-semibold text-gray-900 dark:text-white">Laravel News</h2>
+                            <h2
+                                class="mt-6 text-xl font-semibold text-gray-900 dark:text-white"
+                            >
+                                Laravel News
+                            </h2>
 
-                            <p class="mt-4 text-gray-500 dark:text-gray-400 text-sm leading-relaxed">
-                                Laravel News is a community driven portal and newsletter aggregating all of the latest
-                                and most important news in the Laravel ecosystem, including new package releases and
-                                tutorials.
+                            <p
+                                class="mt-4 text-gray-500 dark:text-gray-400 text-sm leading-relaxed"
+                            >
+                                Laravel News is a community driven portal and
+                                newsletter aggregating all of the latest and
+                                most important news in the Laravel ecosystem,
+                                including new package releases and tutorials.
                             </p>
                         </div>
 
@@ -218,10 +244,17 @@ defineProps({
                                 </svg>
                             </div>
 
-                            <h2 class="mt-6 text-xl font-semibold text-gray-900 dark:text-white">Vibrant Ecosystem</h2>
+                            <h2
+                                class="mt-6 text-xl font-semibold text-gray-900 dark:text-white"
+                            >
+                                Vibrant Ecosystem
+                            </h2>
 
-                            <p class="mt-4 text-gray-500 dark:text-gray-400 text-sm leading-relaxed">
-                                Laravel's robust library of first-party tools and libraries, such as
+                            <p
+                                class="mt-4 text-gray-500 dark:text-gray-400 text-sm leading-relaxed"
+                            >
+                                Laravel's robust library of first-party tools
+                                and libraries, such as
                                 <a
                                     href="https://forge.laravel.com"
                                     class="underline hover:text-gray-700 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500"
@@ -242,8 +275,9 @@ defineProps({
                                     class="underline hover:text-gray-700 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500"
                                     >Envoyer</a
                                 >
-                                help you take your projects to the next level. Pair them with powerful open source
-                                libraries like
+                                help you take your projects to the next level.
+                                Pair them with powerful open source libraries
+                                like
                                 <a
                                     href="https://laravel.com/docs/billing"
                                     class="underline hover:text-gray-700 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500"
@@ -280,8 +314,12 @@ defineProps({
                 </div>
             </div>
 
-            <div class="flex justify-center mt-16 px-6 sm:items-center sm:justify-between">
-                <div class="text-center text-sm text-gray-500 dark:text-gray-400 sm:text-left">
+            <div
+                class="flex justify-center mt-16 px-6 sm:items-center sm:justify-between"
+            >
+                <div
+                    class="text-center text-sm text-gray-500 dark:text-gray-400 sm:text-left"
+                >
                     <div class="flex items-center gap-4">
                         <a
                             href="https://github.com/sponsors/taylorotwell"
@@ -305,8 +343,19 @@ defineProps({
                     </div>
                 </div>
 
-                <div class="ml-4 text-center text-sm text-gray-500 dark:text-gray-400 sm:text-right sm:ml-0">
+                <div
+                    class="ml-4 text-center text-sm text-gray-500 dark:text-gray-400 sm:text-right sm:ml-0"
+                >
                     Laravel v{{ laravelVersion }} (PHP v{{ phpVersion }})
+                    <div v-if="auth.user">
+                        <!-- <div> -->
+                        <button
+                            class="flex mx-auto text-white bg-indigo-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg"
+                        >
+                            テスト
+                        </button>
+                        <p>{{ auth }}</p>
+                    </div>
                 </div>
             </div>
         </div>
